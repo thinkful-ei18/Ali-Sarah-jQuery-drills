c160c0db59291cbe016d91d2d148e27a81c314aa
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-01-18 10:50:10 -0800

    Quick refreactor

--- drills-1.js
+++ drills-1.js
@@ -1,3 +1,4 @@
+'use strict';
 // In this challenge, you need to write JavaScript that provides the following experience: when a user selects one of the thumbnail images using the mouse or keyboard, that image should be displayed in the full - size image container at the top.Updating the image should also update its alt attribute.To test keyboard interactivity, you can Tab to one of the thumbnails and press the Enter key.
 
 // Some of the methods we discussed in the previous assignment might be helpful here, but you should also use the jQuery docs and Google if you're not sure what to do here. Researching on your own is a key skill, and it'll get easier with practice.
@@ -5,9 +6,7 @@
 $(function () {
   //let mainPic = $('.hero');
   $('.thumbnails').click(function(event){
-    console.log('Thumbnail clicked');
-  $('.hero').html(function(event) {
-
-  }) 
-  })
-})
+    $('.hero').html($(event.currentTarget).html());
+    console.log($(this).html());
+  });
+});
